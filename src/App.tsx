6f4c47dc9397416a176e76b43495a106df7ac27b
Zvika Haramaty <haramaty.zvika@gmail.com>
2024-04-18 19:09:08 +0300

    initial: vite, mantine

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
+export default function App() {
+    return <MantineProvider>{
+        <>
+        Hello
+        </>
+    }</MantineProvider>;
+}

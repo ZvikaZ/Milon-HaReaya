422c4b3fcf9286451a2d33d0b5885738af01d535
Zvika Haramaty <haramaty.zvika@gmail.com>
2025-01-02 11:44:52 +0200

    texer: fixed excess spacing between some paragraph groups

--- parser/texer.py
+++ parser/texer.py
@@ -1,8 +1,6 @@
 # TODO meyuan should be in center
-# TODO excess spacing between some paragraph groups דבקות
 # TODO missing spacing between some definitions דין דינים ; זוהר
 # TODO wrong spacing הזרחה
-# TODO? too narrow spacing at end of letter ה
 # TODO too small font definition זהירות הפנימית
 # TODO? unbalanced columns at end of letter ז
 
